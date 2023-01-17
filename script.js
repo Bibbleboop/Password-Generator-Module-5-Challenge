e22ac6ed5e6c241930167dbad32af85872478fe6
Bibbleboop <nicjah@hotmail.com>
2023-01-17 23:45:20 +0000

    draft 1 commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Array of special characters to be included in password
+// Array of special characters to be included in the password 
 var specialCharacters = [
   '@',
   '%',
@@ -101,6 +101,13 @@ function getRandom(arr) {
 // Function to generate password with user input
 function generatePassword() {
 
+
+    let password = '';
+    let length = parseInt(document.Password_Generator.length.value);
+    for(let i = o; i < length; i++) {
+        let random = Math.floor(Math.random() *allowed.length);
+        password += allowed[random];
+    }
 }
 
 // Get references to the #generate element
@@ -108,11 +115,46 @@ var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  let password = generatePassword();
+  let passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+// Asks for number of characters required and expects response in numbers.
+let confirmCharacters = prompt("How many characters do you require?");
+
+// Creates a confirm box which takes in true or false for using numbers.
+let confirmNumbers = confirm("Will you include Numbers?");
+
+// Creates a confirm box which takes in true or false for using symbols.
+let confirmSymbols = confirm("Will you include Symbols?");
+
+
+//// ***********************  this is from sushi question ***
+// // Creates a prompt box which takes in text input.
+// var sushiType = prompt("What kind of sushi do you like?");
+
+// // Alerts the output from our confirm box.
+// alert("You like sushi: " + confirmSushi);
+
+// Alerts the output from our prompt box.
+// alert("You like " + sushiType + " sushi!");
+
+// //    let allowed = '';
+// if (document.Password_Generator.uppercase.checked) {
+//   allowed += uppercase;
+
+// } if (document.Password_Generator.lowercase.checked) {
+//   allowed += lowercase;
+
+// } if (document.Password_Generator.number.checked) {
+//   allowed += number;
+  
+// } if (document.Password_Generator.symbol.checked) {
+//   allowed += symbol;
+  
+// }
