1b61bc40d38e545eedcaabb9a8eb8a6a1fe6e88a
Bibbleboop <nicjah@hotmail.com>
2023-04-02 06:40:21 +0100

    typos

--- script.js
+++ script.js
@@ -87,6 +87,30 @@ var upperCasedCharacters = [
   'Y',
   'Z'
 ];
+// Add event listener to generate button
+// generateBtn.addEventListener('click', writePassword);
+
+// Creates a confirm box which takes in true or false for using numbers.
+let confirmSelection = confirm(" IMPORTANT INFORMATION ABOUT YOUR PASSWORD          Passwords must contain: at least 1 lower [a-z] and 1 UPPER case letter [A-Z], at least 1 numeric character [0-9] & at least 1 special character. Passwords must be at least 10 characters in length");
+
+// Asks for number of characters required and expects response in numbers.
+let confirmCharacters = prompt("How many characters do you require between 10 and 64?");
+
+// Creates a confirm box which takes in true or false for using numbers.
+let confirmNumbers = confirm("Will you include Numbers?");
+
+// Creates a confirm box which takes in true or false for using symbols.
+let confirmSymbols = confirm("Will you include Symbols?");
+
+// Creates a confirm box which takes in true or false for using uppercase.
+let confirmUpper = confirm("Will you include UpperCase?");
+
+// Creates a confirm box which takes in true or false for using lowercase.
+let confirmLower = confirm("Will you include LowerCase?");
+// 
+
+
+
 
 // Function to prompt user for password options
 function getPasswordOptions() {
@@ -101,8 +125,7 @@ function getRandom(arr) {
 // Function to generate password with user input
 function generatePassword() {
 
-
-    let password = '';
+let password = '';
     let length = parseInt(document.Password_Generator.length.value);
     for(let i = o; i < length; i++) {
         let random = Math.floor(Math.random() *allowed.length);
@@ -120,27 +143,40 @@ function writePassword() {
 
   passwordText.value = password;
 }
+//** this is from codepen.io/FlorinPop17/pen/BaBePej **
+// enerate.addEventListener('click', () => {
+// 	const length = +lengthEl.value;
+// 	const hasLower = lowercaseEl.checked;
+// 	const hasUpper = uppercaseEl.checked;
+// 	const hasNumber = numbersEl.checked;
+// 	const hasSymbol = symbolsEl.checked;
+	
+// 	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+// });
 
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+// let passwordOptions = {
+  //   passwordLength: passwordLength;
+  //   hasSpecialCharacters: hasSpecialCharacters;
+  //   hasNumericalCharacters: hasNumbersCharacters;
+  //   hasLowerCaseCharacters: hasLowerCaseCharacters;
+  //   hasUpperCaseCharacters: hasUpperCaseCharacters;
+  
+  // }
+  // // return passwordOptions;
+  
+  // // console.log(passwordOptions);
 
-// Creates a confirm box which takes in true or false for using numbers.
-let confirmSelection = confirm("Important information about your Password!!  **** Passwords must contain: at least 1 lower [a-z] and 1 UPPER case letter [A-Z], at least 1 numeric character [0-9] & at least 1 special character. Passwords must be at least 10 characters in length, but can be much longer");
 
-// Asks for number of characters required and expects response in numbers.
-let confirmCharacters = prompt("How many characters do you require between 10 and 64?");
 
-// Creates a confirm box which takes in true or false for using numbers.
-let confirmNumbers = confirm("Will you include Numbers?");
 
-// Creates a confirm box which takes in true or false for using symbols.
-let confirmSymbols = confirm("Will you include Symbols?");
 
-// Creates a confirm box which takes in true or false for using uppercase.
-let confirmUpper = confirm("Will you include UpperCase?");
 
-// Creates a confirm box which takes in true or false for using lowercase.
-let confirmLower = confirm("Will you include LowerCase?");
+
+
+
+
+
+
 
 
 
